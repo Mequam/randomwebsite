462cc0373579c38b54596ae8fd970e5f5d3ecbd5
Mequam <blue9ja@gmail.com>
2021-03-29 16:46:20 -0400

    creaeted a sql file to hold a basic database

---
+++ sql/create_database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE video(
+vid_Id		SERIAL PRIMARY KEY, --primary key for videos, serial is auto
+vid_upDate	Date,
+vid_source	VarChar(400),
+vid_title	VarChar(200));
+
+CREATE TABLE playlist(
+pla_id	SERIAL PRIMARY KEY,
+pla_title	VarChar(200));
+
+CREATE TABLE playlist2vid(
+pla_id	INT,
+vid_id	INT,
+PRIMARY KEY (pla_id,vid_id),
+CONSTRAINT safe_pla_id
+	FOREIGN KEY(pla_id)
+		REFERENCES playlist(pla_id),
+CONSTRAINT safe_vid_id
+	FOREIGN KEY(vid_id)
+		REFERENCES video(vid_id)
+
+);
